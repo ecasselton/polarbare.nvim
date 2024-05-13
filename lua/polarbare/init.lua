933cbdaaa071bf0dd233f9aad752e1f1841a6331
ecasselton <elliotcelliot@gmail.com>
2024-05-13 16:02:05 +0100

    Main plugin

---
+++ lua/polarbare/init.lua
@@ -0,0 +1,35 @@
+local polarbare = {}
+
+CheckIfDotfile = function(git_dir, work_tree)
+	local command = "/usr/bin/git --git-dir="..git_dir.." --work-tree="..work_tree.." ls-files --error-unmatch "..vim.api.nvim_buf_get_name(0)
+
+	vim.fn.system(command)
+	local exit_code = vim.v.shell_error
+
+	if exit_code == 0 then
+		return true
+	else
+		return false
+	end
+end
+
+polarbare.setup = function(opts)
+	local default_opts = {
+		git_dir = "~/.dotfiles",
+		work_tree = "~",
+	}
+
+	local options = vim.tbl_deep_extend("force", default_opts, opts)
+
+	options.git_dir = vim.fn.expand(options.git_dir)
+	options.work_tree = vim.fn.expand(options.work_tree)
+
+	-- print(vim.inspect(options))
+
+	if CheckIfDotfile(options.git_dir, options.work_tree) then
+		vim.env.GIT_DIR = options.git_dir
+		vim.env.GIT_WORK_TREE = options.work_tree
+	end
+end
+
+return polarbare
