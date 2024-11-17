f16e4791f95ebab786975ae1aca558dd3dc61128
ecasselton <elliotcelliot@gmail.com>
2024-11-17 22:03:12 +0000

    Made opts an optional argument

--- lua/polarbare/init.lua
+++ lua/polarbare/init.lua
@@ -1,7 +1,7 @@
 local polarbare = {}
 
 CheckIfDotfile = function(git_dir, work_tree)
-	buf_name = vim.api.nvim_buf_get_name(0)
+	local buf_name = vim.api.nvim_buf_get_name(0)
 	if buf_name == "" then
 		return false
 	end
@@ -19,12 +19,10 @@ polarbare.setup = function(opts)
 		work_tree = "~",
 	}
 
-	local options = vim.tbl_deep_extend("force", default_opts, opts)
+	local options = vim.tbl_deep_extend("force", default_opts, opts or {})
 
 	options.git_dir = vim.fn.expand(options.git_dir)
 	options.work_tree = vim.fn.expand(options.work_tree)
-
-	-- print(vim.inspect(options))
 
 	if CheckIfDotfile(options.git_dir, options.work_tree) then
 		vim.env.GIT_DIR = options.git_dir
